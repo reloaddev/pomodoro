547993da4d951d5c901d0d03c08a09e149eda798
reloaddev <henrik.pruess@posteo.de>
2024-05-16 11:15:01 +0200

    Fix build

--- src/app/timer/buttons/timer-start-btn.tsx
+++ src/app/timer/buttons/timer-start-btn.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 export default function TimerStartBtn({startTimer}: { startTimer: () => void }) {
-    let audio = new Audio("/click.mp3");
-    const start = () => audio.play();
+    let audio: any;
+    if (typeof window !== 'undefined') {
+        audio = new Audio("/click.mp3");
+    }
+    const start = () => void audio.play();
 
     return (
         <button className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
